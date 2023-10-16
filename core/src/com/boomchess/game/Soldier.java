6456640589ac4b4c16ca5c36810f58a8d70f4338
Marty Lauterbach <LauterbachMK@protonmail.com>
2023-10-16 12:37:53 +0200

    updated Soldier to use Coordinates for better work-a-like with tile Widgets of Scene2DUI

--- core/src/com/boomchess/game/Soldier.java
+++ core/src/com/boomchess/game/Soldier.java
@@ -13,6 +13,7 @@ public class Soldier {
     private String teamColor;
     private int pieceID;
     private int health;
+    private Coordinates coordinates;
 
     public Soldier(boolean taken, String type, String Color, int ID, int health) {;
         this.isTaken = taken;
@@ -20,6 +21,16 @@ public class Soldier {
         this.teamColor = Color;
         this.pieceID = ID;
         this.health = health;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public Coordinates setCoordinates(int x, int y) {
+        Coordinates coordinates = new Coordinates();
+        coordinates.setCoordinates(x, y);
+        return this.coordinates;
     }
 
     public boolean getTaken() {
