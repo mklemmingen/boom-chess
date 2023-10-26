24b207224a5edc699edb084747cb78cde218bead
Marty Lauterbach <LauterbachMK@protonmail.com>
2023-10-26 20:37:26 +0200

    updated MusicPlaylist.java to play random songs after one has finished

--- core/src/com/boomchess/game/frontend/sound/MusicPlaylist.java
+++ core/src/com/boomchess/game/frontend/sound/MusicPlaylist.java
@@ -8,30 +8,59 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Playlist {
-    private List<Song> songs;
-    private int currentIndex;
-    private boolean isLooping;
+public class MusicPlaylist {
+    /*
+     * MusicPlaylist.java is the object for the music playlist in the game Boom Chess.
+     * It holds the playlist of music and the methods to play, pause, resume, and dispose of the music.
+     */
+    private final List<Music> songs;
+    private int currentIndex = 0;
+    private boolean isLooping = false;
 
-    public Playlist() {
+    public MusicPlaylist() {
+        /*
+        * Constructor for MusicPlaylist.java
+        * does not take any arguments.
+        */
         songs = new ArrayList<>();
-        currentIndex = 0;
-        isLooping = false;
     }
 
-    public void addSong(Song song) {
+    public void addSong(String fileName) {
+        /*
+        * addSong adds a song, given a fileName/Direction to a Music Object, to the playlist.
+        * Adds a listener to go to the next song when the current song is finished.
+         */
+        Music song = Gdx.audio.newMusic(Gdx.files.internal(fileName));
+        song.setOnCompletionListener(new Music.OnCompletionListener() {
+            @Override
+            public void onCompletion(Music music) {
+                nextSong();
+            }
+        });
         songs.add(song);
-    }
-
-    public void setLooping(boolean looping) {
-        isLooping = looping;
     }
 
     public void play() {
         if (songs.size() > 0) {
+            Music playSong = songs.get(currentIndex);
+            playSong.play();
+            playSong.setVolume(BoomChess.volume);
+        }
+    }
+
+    public void pause() {
+        if (songs.size() > 0) {
+            songs.get(currentIndex).pause();
+        }
+    }
+
+    public void resume() {
+        if (songs.size() > 0) {
             songs.get(currentIndex).play();
         }
     }
+
+
 
     public void nextSong() {
         /*
@@ -48,6 +77,42 @@ public class Playlist {
             } while (currentIndex == previousIndex);
 
             play();
+        }
+    }
+
+    public void stop() {
+        if (songs.size() > 0) {
+            songs.get(currentIndex).stop();
+        }
+    }
+
+    public void setVolume(float volume){
+        // can take but must not take a int value, if there is one, it is taken as the volume for setVolume
+        if (songs.size() > 0) {
+            songs.get(currentIndex).setVolume(volume);
+        }
+    }
+
+    public void setLooping(boolean isLooping){
+        /*
+        * setLooping never lets the playlist end. It loops the playlist.
+         */
+        this.isLooping = isLooping;
+    }
+
+    public boolean isPlaying() {
+    /*
+    * isPlaying returns a boolean value of whether a song is playing.
+    */
+        if (songs.size() > 0) {
+            return songs.get(currentIndex).isPlaying();
+        }
+        return false;
+    }
+
+    public void dispose() {
+        for (Music song : songs) {
+            song.dispose();
         }
     }
 }
