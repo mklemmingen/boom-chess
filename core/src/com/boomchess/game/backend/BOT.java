7d66c0648b2a459d7dabd7d00066665f7cbe039d
Marty Lauterbach <LauterbachMK@protonmail.com>
2023-10-29 11:58:03 +0100

    added BOT class skeleton

---
+++ core/src/com/boomchess/game/backend/BOT.java
@@ -0,0 +1,32 @@
+package com.boomchess.game.backend;
+
+public class BOT {
+    /*
+    * BOT.java is the object for the bot move calculations by difficulty in the game Boom Chess.
+     */
+
+    public void easyBotMove(){
+        /*
+        * this method completes a random move for the bot
+        * it gives out a random move for the bot, trying to move all soldiers as close as possible to the enemy general
+         */
+
+    }
+
+    public void mediumBotMove(){
+        /*
+        * this method completes a random move for the bot
+        * It tries to move a soldier to a position where the overall damage to the opponent is maximised
+         */
+
+    }
+
+    public void hardBotMove(){
+        /*
+        * this method completes a random move for the bot
+        * It tries to move all soldiers as close as possible to the general while maximising
+        * the overall damage to the opponent
+         */
+
+    }
+}
