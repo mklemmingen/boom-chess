e92cd473081898aac2fe08a5e3ebd7d84f735985
Marty (mklemmingen) <LauterbachMK@protonmail.com>
2023-11-09 20:57:49 +0100

    added functioning mediumBot

--- core/src/com/boomchess/game/backend/BOT.java
+++ core/src/com/boomchess/game/backend/BOT.java
@@ -150,10 +150,80 @@ public class BOT {
         * as well as no restrictions for war dog and commando movement
          */
 
-        // TODO implement pathfinding
+        // TODO ADD PATHFINDING TO DISTINGUISH FROM MEDIUM BOT
 
         Soldier[][] gameBoard = Board.getGameBoard();
 
+        int numRows = 8;
+        int numColumns = 9;
+
+        // these values hold the current max score, the coordinates of the Soldier that has the max score,
+        // and the coordinates of the move that has the max score
+        int maxScore = 0;
+        Coordinates maxScoreSoldier = new Coordinates();
+        Coordinates maxScoreMove = new Coordinates();
+
+        // -----------------------------------------------------------------------------------
+        // Get the location of the enemy general
+        int enemyGeneralX = 0;
+        int enemyGeneralY = 0;
+        int friendlyGeneralX = 0;
+        int friendlyGeneralY = 0;
+
+        // loop through gameBoard for enemy general
+        for (int i = 0; i < 9; i++) { // X Coordinate Loop
+            for (int j = 0; j < 8; j++) { // Y coordinate loop
+                if (gameBoard[i][j] instanceof General) {
+                    if (gameBoard[i][j].getTeamColor().equals("green")) {
+                        enemyGeneralX = i;
+                        enemyGeneralY = j;
+                    } else {
+                        friendlyGeneralX = i;
+                        friendlyGeneralY = j;
+                    }
+                }
+            }
+
+        }
+
+        // logic for going through the 2DArray
+        // loop through all the soldiers on the board and get their possible moves
+        // loop through all the soldiers on the board and get their possible moves
+        for (int i = 0; i < numColumns; i++) {
+            for (int j = 0; j < numRows; j++) {
+                if (gameBoard[i][j].getTeamColor().equals("red")){
+                    // create List of all Moves
+                    ArrayList<Coordinates> moves = gameBoard[i][j].mathMove(i, j);
+                    // loop through the created list
+                    // for each move, evaluate the move
+                    for(Coordinates move : moves){
+
+                        int moveX = move.getX();
+                        int moveY = move.getY();
+
+                        int score;
+                        score = DrMoveJudge.evaluateMove(i, j, moveX, moveY, enemyGeneralX, enemyGeneralY,
+                                friendlyGeneralX, friendlyGeneralY);
+
+                        // if the move has a higher score than the current max score, update the max score
+                        // and the Coordinates of the Soldier and the move
+
+                        if (score > maxScore){
+                            maxScore = score;
+                            maxScoreSoldier.setCoordinates(i, j);
+                            maxScoreMove.setCoordinates(moveX, moveY);
+                        }
+                    }
+                }
+            }
+        }
+
+        int SX = maxScoreSoldier.getX();
+        int SY = maxScoreSoldier.getY();
+        int x = maxScoreMove.getX();
+        int y = maxScoreMove.getY();
+
+        moveSoldierTo(SX, SY, x, y);
     }
 
     public static void moveSoldierTo(int SX, int SY, int x, int y){
